5ced21896dd7383c0745f1d66fa3a8b9b35c16c3
Siddarth2904 <bsiddarth729@gmail.com>
2026-07-25 23:55:46 +0530

    Configured RabbitMQ

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/services/ActivityService.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/services/ActivityService.java
@@ -4,19 +4,30 @@ import com.fitness.activityservice.dto.ActivityRequest;
 import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.model.Activity;
 import com.fitness.activityservice.repositories.ActivityRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.Nullable;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Slf4j
 @Service
 public class ActivityService {
     @Autowired
     private ActivityRepository activityRepository;
     @Autowired
     private UserValidationService userValidationService;
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
     public ActivityResponse trackActivity(ActivityRequest request){
         Boolean isValidUser=userValidationService.validateUser(request.getUserId());
         if(!isValidUser){
@@ -33,6 +44,11 @@ public class ActivityService {
         Activity savedActivity = activityRepository.save(activity);
         System.out.println("Activities:");
         System.out.println(activityRepository.findAll());
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, savedActivity);
+        }catch (Exception e) {
+            log.error("Error while sending activity to RabbitMQ: ",e);
+        }
         return mapToResponse(savedActivity);
     }
     public ActivityResponse mapToResponse(Activity activity){
